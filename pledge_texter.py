1176c75c41c5d409284687418831b48265c31f15
Alexander Large <alexanderklarge@gmail.com>
2019-10-17 15:53:37 +0100

    Pledge texter works!
    It will text me every day at the same time.
    
    Will leave the pledge_texter.py script running on a Raspberry Pi.

--- pledge_texter.py
+++ pledge_texter.py
@@ -1,6 +1,9 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
+import schedule
+import time
 
+### Define pledges
 print('What are your pledges for this week?')
 user_pledges = str(input())
 
@@ -10,13 +13,18 @@ account_sid = 'AC9a17dd2a160a15c4b5ad4db719e0f4d1' # good to make these secret
 auth_token = '4d645e040cc08e6bf6997a4b0e0a2e54' # use environment variables to make secret!
 client = Client(account_sid, auth_token)
 
-# Want to get it to send me a text every day at 8am
+### schedule module code
+# from here https://stackoverflow.com/questions/15088037/python-script-to-do-something-at-the-same-time-every-day
+def text_me():
+	message = client.messages \
+    	.create(
+         	body='Hello! You pledged the following this week: {}'.format(user_pledges),
+         	from_='+16193562670',
+         	to='+447397854011'
+     	)
 
-message = client.messages \
-    .create(
-         body='Hello! You pledged the following this week: {}'.format(user_pledges),
-         from_='+16193562670',
-         to='+447397854011'
-     )
+schedule.every().day.at("15:48").do(text_me)
 
-print(message.sid)
+while True:
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
